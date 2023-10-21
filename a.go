4def613c38f12a1ebb029a2d20c6b144b3f677e2
fischerman <code@bjorn-fischer.de>
2023-10-21 13:53:03 +0200

    add comment about dead code

--- a.go
+++ a.go
@@ -9,6 +9,7 @@ import (
 func f() (*loadbalancer.CreateLoadBalancerPayload, error) {
 	return nil, fmt.Errorf("test")
 
+	// Dead code must be present to provoke the invalid stack pointer.
 	b := []string{"a", "b", "c"}
 
 	for i := range b {
